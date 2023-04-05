182774715d3254491e2a323cd3de76b861a195b6
Cosmo Alan Pires Lira <105260581+cosmoalanpireslira@users.noreply.github.com>
2023-04-05 16:20:17 -0300

    Create Professor.php

---
+++ Professor.php
@@ -0,0 +1,11 @@
+<?php
+class Professor{
+  public $nome;
+  public $disciplina;
+
+  public function exibirProfessor(){
+    echo "\nProfessor: ".$this->nome. "\nDisciplina: ".$this->disciplina;
+
+  }
+
+}
